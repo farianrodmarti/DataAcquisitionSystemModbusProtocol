3d9a654e285e8b8f8ba67c64e92ee89d39149ea0
faria <faria@FARIAN>
2024-09-05 04:30:33 -0400

    Servicios listos

--- GrpcService1/Program.cs
+++ GrpcService1/Program.cs
@@ -60,3 +60,17 @@ namespace GrpcService1
     }
 
 }
+
+/* 
+            //builder.Services.AddScoped<IPriceRepository, ApplicationRepository>();
+            
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.MapGrpcService<MotorcycleService>();
+
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.Run();
+        }
+*/
